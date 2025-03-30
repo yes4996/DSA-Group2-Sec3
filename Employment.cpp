93bb13eb290f5b6293cbe9a8ce052b9a89040e7e
yes4996 <shumetyeserah8@gmail.com>
2025-03-30 20:30:50 +0300

    Update Employment.cpp

--- Employment.cpp
+++ Employment.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+
+using namespace std;
+
+const int MAX_EMPLOYEES = 100;
+
+struct Employee {
+    int id;
+    string name;
+    string position;
+    double dailyWage;
+};
+
+struct AttendanceNode {
+    int employeeID;
+    int day;            
+    bool present;
+    AttendanceNode* next;
+};
